1423f7fa86b2360190ae82e72e1808049b2f1d5d
Kioto Hirahara <kioto.hirahara@gmail.com>
2021-04-11 16:06:55 +0900

    Modify DISPLAY IP address setting

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
-docker run -it -d --name ubuntu_python-1 -p 8889:8888 ^
-       -v %~dp0\work:/root/share ubuntu_20_04_python
+FOR /F %%i in ('python -c "import socket; print(socket.gethostbyname(socket.gethostname()))"') DO @SET IPADDR=%%i
+
+docker run -it -d --name ubuntu_pybullet-1 -p 8889:8888 ^
+	-e DISPLAY=%IPADDR%:0.0 ^
+	-v %~dp0/work:/home/devuser/share ^
+       	ubuntu_pybullet
